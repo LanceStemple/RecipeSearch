3cdc9d6adbfb8e80963771e0d203c94a24716b00
LanceStemple <stemplel@msu.edu>
2024-04-06 18:45:20 -0400

    Testing pushing again

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Testing haha
+          Change file to test
         </a>
       </header>
     </div>
