af4da09ff969eb04c8c11a7c03e1fb45acbf9ada
LanceStemple <stemplel@msu.edu>
2024-04-06 22:15:34 -0400

    Setup for api key (ignore in github and use secrets instead)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 function Header(){
     return(
         <div className="header bg-black w-100 d-flex justify-content-center align-items-center">
-            <h2 className="text-white">TESTING HEADER HERE</h2>
+            <h2 className="text-white">Recipe Search</h2>
         </div>
     )
 }
