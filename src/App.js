9bab84d3c18dc6fc27f4b0f04f440c36c0ba7adb
LanceStemple <stemplel@msu.edu>
2024-04-06 21:55:57 -0400

    More setup and very basic header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header/Header.js'
 
 function App() {
   return (
     <div>
-      
+      <Header></Header>
+      <h1>Testing</h1>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header(){
+    return(
+        <div className="header bg-black w-100 d-flex justify-content-center align-items-center">
+            <h2 className="text-white">TESTING HEADER HERE</h2>
+        </div>
+    )
+}
+
+export default Header;
