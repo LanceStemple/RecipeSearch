2041beebf90015a5971388abad5d16ab2d5c1256
LanceStemple <stemplel@msu.edu>
2024-04-13 19:37:30 -0400

    Update to simply show each result

--- src/pages/ResultsPage/ResultsPage.js
+++ src/pages/ResultsPage/ResultsPage.js
@@ -1,29 +1,52 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import Header from '../../components/Header/Header.js'
 
 function ResultsPage() {
   const location = useLocation();
+  const [recipes, setRecipes] = useState([]);
+
   const result = location.state ? location.state.result : null;
 
   useEffect(() => {
-    if (result) {
-      setRecipe(result);
+    if (location.state && location.state.result) {
+      setRecipes(location.state.result.hits.map(hit => hit.recipe));
     }
-  }, [result]); // Only re-run the effect if result changes
+  }, [location.state]);
 
   function setRecipe(result) {
-    document.getElementsByClassName('label')[0].innerHTML = result.hits[0].recipe.label;
-    document.getElementsByClassName('url')[0].innerHTML = result.hits[0].recipe.url;
+    let labels = []
+    result.hits.forEach((hit) =>{
+      labels.push(hit.recipe.label);
+    });
+
+    let urls = []
+    result.hits.forEach((hit) =>{
+      urls += hit.recipe.url;
+    });
   }
 
   return (
     <div>
         <Header activeNavItem="search"/>
         <div>
-            <h2 className='label'>LABEL</h2>
-            <h2 className='url'>URL</h2>
+            <table className='w-100'>
+              <thead>
+                <tr>
+                  <th>Label</th>
+                  <th>URL</th>
+                </tr>
+              </thead>
+              <tbody>
+                {recipes.map((recipe, index) => (
+                <tr key={index}>
+                  <td>{recipe.label}</td>
+                  <td><a href={recipe.url} target="_blank" rel="noopener noreferrer">{recipe.url}</a></td>
+                </tr>
+              ))}
+              </tbody>
+            </table>
         </div>
     </div>
   );
